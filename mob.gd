5c5a1cd33fc16d4a3c5ea6f25090497beb870c5c
sufei <sufei@weichengya.cn>
2026-06-26 14:45:56 +0800

    init

---
+++ mob.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
+	$AnimatedSprite2D.animation = mob_types.pick_random()
+	$AnimatedSprite2D.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	# make the mobs delete themselves when they leave the screen.
+	# queue_free() is a function that essentially 'frees', or deletes, the node at the end of the frame.
+	queue_free()
